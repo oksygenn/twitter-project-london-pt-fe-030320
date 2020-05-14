f14113d49ac80afa035fd4da1b6f5a92b0e63281
Oksana Kostenko <kseniavlad31@gmail.com>
2020-05-14 16:30:49 +0100

    login page background

--- src/API.js
+++ src/API.js
@@ -1,9 +1,9 @@
-const API_ENDPOINT = "http://localhost:3000"
-const USERS_URL = `${API_ENDPOINT}/users?_embed=tweets`
-const TWEETS_URL = `${API_ENDPOINT}/tweets?_expand=user&_embed=comments`
+const API_ENDPOINT = "http://localhost:3000";
+const USERS_URL = `${API_ENDPOINT}/users?_embed=tweets`;
+const TWEETS_URL = `${API_ENDPOINT}/tweets?_expand=user&_embed=comments`;
 
-const getTweets = () => fetch(TWEETS_URL).then(res => res.json())
+const getTweets = () => fetch(TWEETS_URL).then((res) => res.json());
 
 export default {
-    getTweets
-}
+  getTweets,
+};
