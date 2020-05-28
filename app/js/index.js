9459d4cbdda7e0b4739f26b8558a57e9c3d0614b
Oksana Kostenko <kseniavlad31@gmail.com>
2020-05-28 13:31:57 +0100

    changes in createTweet

--- app/js/index.js
+++ app/js/index.js
@@ -16,51 +16,15 @@ const loadTweets = async () => {
 
 const createTweet = () => {
   for (let obj of tweets) {
-    const tweetDiv = document.createElement("div");
-    tweetDiv.className = "tweet";
-    tweetDiv.innerHTML = `
-    <div class="tweet-header">
-    <h4 class="tweet-name">${obj.userId}</h4>
-    <p class="tweet-date">
-    ${obj.date}</p>
-  </div>
-  <div class="tweet-body">
-    <p class="tweet-content">${obj.content}
-    </p>
-    <div class="tweet-line"></div>
-  </div>
-  <div class="tweet-footer">
-    <div class="tweet-buttons">
-      <div class="like-button">
-        <img src="images/heart.svg" alt="like icon" />
-        <p class="like-quantity">${obj.likes}</p>
-      </div>
-      <div class="retweet-button">
-        <img src="images/retweet.svg" alt="retweet icon" />
-        <p class="retweet-quantity">${obj.retweets}</p>
-      </div>
-      <div class="comment-button">
-        <img src="images/comment.svg" alt="comment icon" />
-        <p class="comment-quantity">0</p>
-      </div>
-    </div>
-  </div>
+    const template = document.querySelector(".tweet");
+    const tweetDiv = template.cloneNode(true);
+    tweetDiv.classList.remove("template");
+    tweetDiv.querySelector(".tweet-name").innerHTML = obj.userId;
+    tweetDiv.querySelector(".tweet-date").innerHTML = obj.date;
+    tweetDiv.querySelector(".tweet-content").innerHTML = obj.content;
+    tweetDiv.querySelector(".like-quantity").innerHTML = obj.likes;
+    tweetDiv.querySelector(".retweet-quantity").innerHTML = obj.retweets;
 
-  <div class="tweet-comment-field hidden">
-    <div class="comment-line"></div>
-    <textarea
-      type="text"
-      cols="34"
-      rows="5"
-      placeholder="Leave your comment here"
-    ></textarea
-    ><br />
-    <div class="tweet-comment-buttons">
-      <img src="images/back-arrow.svg" alt="" />
-      <input type="submit" value="Reply" />
-    </div>
-  </div>
-`;
     tweetsContainer.appendChild(tweetDiv);
 
     const commentBtn = tweetDiv.querySelector(".comment-button");
